e3ea7158cc3ead7009e53d39d0ed702c78671b7a
Chris Gordon <chrisgordondev998@gmail.com>
2019-01-25 16:48:10 +0000

    Add movie preview boxes
    Signed-off-by: Chris Gordon <chrisgordondev998@gmail.com>

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const path = require('path');
+const movies = require('./movies.json');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('pages/index');
+  res.render('pages/index', { movies: movies });
 });
 
 app.listen(port, () => {
